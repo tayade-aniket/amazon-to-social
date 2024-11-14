135b6e610b07960934bbb49a7a8f86dd19e09087
Aniket Tayade <156310472+tayade-aniket@users.noreply.github.com>
2024-11-14 23:14:05 +0530

    commit

--- client/src/components/ProductList.jsx
+++ client/src/components/ProductList.jsx
@@ -7,7 +7,7 @@ function ProductList() {
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-        const response = await fetch("http://localhost:5000/api/products");
+        const response = await fetch("https://amazon-to-social-rdjb.vercel.app/api/products");
         if (!response.ok) {
           throw new Error("Network response was not ok");
         }
